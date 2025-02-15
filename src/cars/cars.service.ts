b95b69f44c415c06710b7f2e5eb55db3298efa4f
FernandoIF <fernando.iftm@outlook.com>
2025-02-15 09:31:12 -0300

    API de teste Finalizada

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
+import { Car } from './entities/car.entity';
+
+@Injectable()
+export class CarsService {
+  private readonly cars: Car[] = [];
+  private id = 1;
+
+  create(createCarDto: CreateCarDto) {
+    const newCar = {
+      id: this.id,
+      brand: createCarDto.brand,
+      model: createCarDto.model,
+      year: createCarDto.year
+    }
+
+    this.id++;
+    this.cars.push(newCar);
+
+    return newCar; 
+  }
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOne(id: number) {
+   const car = this.cars.find((car) => car.id == id);
+
+   if(!car){
+    throw new NotFoundException("Carro não encontrado");
+   }
+
+   return car;
+  }
+
+  update(id: number, updateCarDto: UpdateCarDto) {
+    const car = this.findOne(id);
+
+    car.brand = updateCarDto.brand ?? "";
+    car.model = updateCarDto.model ?? "";
+    car.year = updateCarDto.year ?? 0;
+
+    return `This action updates a #${id} car`;
+  }
+
+  remove(id: number) {
+    this.findOne(id);
+
+    const carIndex = this.cars.findIndex((car) => car.id == id);
+    this.cars.splice(carIndex, 1)
+
+    return;
+  }
+}
